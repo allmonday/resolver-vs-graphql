2eb2bbf0e4593a40bea3fbcd3488a9c3d40560a7
tangkikodo <allmonday@126.com>
2025-06-29 15:49:05 +0800

    fix conflicts

--- app_bench/main.py
+++ app_bench/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .graphql import graphql_app
-from .rest import router as rest_router
-from .rest_dataclass import router as rest_dc_router
+from .resolver import router as rest_router
+from .resolver_dataclass import router as rest_dc_router
 
 app = FastAPI()
 app.include_router(graphql_app, prefix="/graphql")
